93a29009f4b248e8f65340c98901a9e373568f74
wukong <wukonggg@139.com>
2016-06-24 20:39:35 +0800

    SerializeUtil

---
+++ src/main/java/band/wukong/util/SerializeUtil.java
@@ -0,0 +1,73 @@
+package band.wukong.util;
+
+import java.io.*;
+
+/**
+ * As you see...
+ *
+ * @author wukong(wukonggg@139.com)
+ */
+public class SerializeUtil {
+
+    public static byte[] serial(Object object) {
+        ByteArrayOutputStream baos = null;
+        ObjectOutputStream oos = null;
+        try {
+            baos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(baos);
+            oos.writeObject(object);
+
+            return baos.toByteArray();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.close();
+                }
+            } catch (IOException ignored) {
+                ignored.printStackTrace();
+            }
+            try {
+                if (baos != null) {
+                    baos.close();
+                }
+            } catch (IOException ignored) {
+                ignored.printStackTrace();
+            }
+        }
+    }
+
+    public static Object deserial(byte[] bytes) {
+        ByteArrayInputStream bais = null;
+        ObjectInputStream ois = null;
+
+        try {
+            bais = new ByteArrayInputStream(bytes);
+            ois = new ObjectInputStream(bais);
+
+            return ois.readObject();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+
+        } finally {
+            try {
+                if (bais != null) {
+                    bais.close();
+                }
+            } catch (IOException ignored) {
+                ignored.printStackTrace();
+            }
+            try {
+                if (ois != null) {
+                    ois.close();
+                }
+            } catch (IOException ignored) {
+                ignored.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/test/java/band/wukong/util/SerializeUtilTest.java
@@ -0,0 +1,24 @@
+package band.wukong.util;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * As you see...
+ *
+ * @author wukong(wukonggg@139.com)
+ */
+public class SerializeUtilTest {
+
+    @Test
+    public void serial_and_deserial() throws IOException {
+        String name = new File("./README.MD").getName();
+        System.out.println(name);
+        byte[] bytes = SerializeUtil.serial(name);
+        System.out.println((String)SerializeUtil.deserial(bytes));
+    }
+}
